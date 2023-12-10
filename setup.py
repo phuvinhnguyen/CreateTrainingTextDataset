1284bb84ecaff6f05ae5e934bc47948d1a4d07ad
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2023-12-10 17:55:00 +0700

    update package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='processTrainingText',
+    name='processtrainingtext',
     version='0.1',
     packages=find_packages(),
     install_requires=[
